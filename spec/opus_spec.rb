142fd4503e49a292813946c4c1a3bb4602f46b7b
Sharon Rosner <ciconia@gmail.com>
2015-04-24 23:14:25 +0200

    Refactor basic example.

--- spec/opus_spec.rb
+++ spec/opus_spec.rb
@@ -3,7 +3,7 @@ require File.join(File.expand_path(File.dirname(__FILE__)), 'spec_helper')
 RSpec.describe Lydown::Opus do
   context "with a clean context" do
     before(:context) do
-      @source = load_example('example1.ld')
+      @source = load_example('example-simple.ld')
     end
     
     before(:example) do
@@ -13,13 +13,13 @@ RSpec.describe Lydown::Opus do
     it "compiles correctly to raw" do
       @opus.compile(@source)
       ly = @opus.to_lilypond(stream_path: 'movements//parts//music')
-      expect(ly.strip_whitespace).to eq(load_example('example1-raw.ly'))
+      expect(ly.strip_whitespace).to eq(load_example('example-simple-raw.ly'))
     end
 
     it "compiles correctly to lilypond document" do
       @opus.compile(@source)
       ly = @opus.to_lilypond
-      expect(ly.strip_whitespace).to eq(load_example('example1.ly'))
+      expect(ly.strip_whitespace).to eq(load_example('example-simple.ly'))
     end
   end
 end
