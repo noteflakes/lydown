00061e7b3b82519a060fa540a4ba1549fb77fd16
Sharon Rosner <ciconia@gmail.com>
2015-07-22 20:55:07 +0200

    Spec & implement translation of whole directories
    Close #2.

--- lib/lydown/cli/translation.rb
+++ lib/lydown/cli/translation.rb
@@ -1,4 +1,6 @@
-module Lydown::CLI::Compiler
+require 'yaml'
+
+module Lydown::CLI::Translation
   class << self
     def process(opts)
       if File.directory?(opts[:path])
@@ -7,6 +9,17 @@ module Lydown::CLI::Compiler
         process_file(opts[:path])
       elsif File.file?(opts[:path] + '.rpl')
         process_file(opts[:path] + '.rpl')
+      end
+    end
+    
+    def process_directory(path)
+      macros = load_macros(path)
+      Dir["#{path}/*"].entries.each do |entry|
+        if File.file?(entry) && (entry =~ /\.rpl$/)
+          process_file(entry)
+        elsif File.directory?(entry)
+          process_directory(entry)
+        end
       end
     end
     
@@ -20,12 +33,38 @@ module Lydown::CLI::Compiler
       code = {
         ripple: IO.read(path),
         lyrics: File.file?(lyrics_path) && IO.read(lyrics_path),
-        figures: File.file?(figures_path) && IO.read(figures_path)
+        figures: File.file?(figures_path) && IO.read(figures_path),
+        macros: macros_for_path(path)
       }
       
       ld_code = Lydown::Translation.process(code)
 
       File.open(output_path, 'w+') {|f| f.write(ld_code)}
+    end
+    
+    WORK_FILENAME = '_work.yml'
+    MOVEMENT_FILENAME = '_movement.yml'
+    
+    PATH_MACROS = {}
+    
+    def macros_for_path(path)
+      PATH_MACROS[path] ||= load_macros(path)
+    end
+    
+    def load_macros(path)
+      base_dir = File.directory?(path) ? path : File.dirname(path)
+      parent_dir = File.expand_path(File.join(base_dir, '..'))
+      
+      yml = 
+        (YAML.load_file(File.join(parent_dir, WORK_FILENAME)) rescue nil) || 
+        (YAML.load_file(File.join(base_dir, WORK_FILENAME)) rescue nil) ||
+        {}
+      macros = yml['macros'] || {}
+      
+      yml = 
+        (YAML.load_file(File.join(base_dir, MOVEMENT_FILENAME)) rescue nil) ||
+        {}
+      macros.merge(yml['macros'] || {})
     end
   end
 end
--- spec/translation_spec.rb
+++ spec/translation_spec.rb
@@ -1,4 +1,6 @@
 require File.join(File.expand_path(File.dirname(__FILE__)), 'spec_helper')
+require 'fileutils'
+require 'lydown/cli'
 
 RSpec.describe Lydown::Translation do
   it "translates simple ripple code to lydown" do
@@ -12,5 +14,17 @@ RSpec.describe Lydown::Translation do
       macros: {'m1' => '#6. #3', 'm5' => '#4 ~ @6. #3 #6. #3'}
     )
     expect(ld).to eq(load_example('translation_macros.ld'))
+  end
+
+  it "translates a complete directory of ripple files" do
+    FileUtils.rm('spec/examples/translate/mvmt1/*.ld') rescue nil
+    ld = Lydown::CLI::Translation.process(
+      path: File.join('spec/examples/translate')
+    )
+    
+    basso = IO.read('spec/examples/translate/mvmt1/basso.ld')
+    violino1 = IO.read('spec/examples/translate/mvmt1/violino1.ld')
+    expect(basso).to eq(load_example('translation_simple.ld'))
+    expect(violino1).to eq(load_example('translation_macros.ld'))
   end
 end
