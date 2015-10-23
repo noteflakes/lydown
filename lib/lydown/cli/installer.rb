ff1ac0fb81a92e862aa97d7772e16142472e5ee4
Sharon Rosner <ciconia@gmail.com>
2015-10-23 22:06:22 +0200

    Add support for installing lilypond on linux (#52)
    Should verify that it’s indeed working

--- lib/lydown/cli/installer.rb
+++ lib/lydown/cli/installer.rb
@@ -9,105 +9,145 @@ module Lydown::CLI::Installer
     def install(package, version = nil)
       case package
       when 'lilypond'
-        install_lilypond(version)
+        Lilypond.install(version)
       else
         STDERR.puts "Unknown package name specified"
         exit!(1)
       end
     end
-    
-    LILYPOND_DEFAULT_VERSION = "2.19.29"
-    
-    def install_lilypond(version = nil)
-      version ||= LILYPOND_DEFAULT_VERSION      
-      platform = detect_lilypond_platform
-      url = lilypond_install_url(platform, version)
-      fn = Tempfile.new('lydown-lilypond-installer').path
-
-      download_lilypond(url, fn)
-      install_lilypond_files(fn, platform, version)
-    end
-    
-    BASE_URL = "http://download.linuxaudio.org/lilypond/binaries"
-    
-    def lilypond_install_url(platform, version)
-      ext = platform =~ /darwin/ ? ".tar.bz2" : ".sh"
-      filename = "lilypond-#{version}-1.#{platform}"
+  end
+  
+  module Lilypond
+    class << self
+      LILYPOND_DEFAULT_VERSION = "2.19.29"
       
-      "#{BASE_URL}/#{platform}/#{filename}#{ext}"
-    end
-    
-    def detect_lilypond_platform
-      case RUBY_PLATFORM
-      when /x86_64-darwin/
-        "darwin-x86"
-      when /ppc-darwin/
-        "darwin-ppc"
-      when "i686-linux"
-        "linux-x86"
-      when "x86_64-linux"
-        "linux-64"
-      when "ppc-linux"
-        "linux-ppc"
+      def detect_version(specified_version)
+        case specified_version
+        when nil, 'stable'
+          list = get_version_list
+        end
       end
-    end
     
-    def download_lilypond(url, fn)
-      STDERR.puts "Downloading #{url}"
-      
-      url_base = url.split('/')[2]
-      url_path = '/'+url.split('/')[3..-1].join('/')
-      @counter = 0
+      def install(version = nil)
+        platform = detect_lilypond_platform
+        version ||= LILYPOND_DEFAULT_VERSION
+        # version = detect_version(version)
+        url = lilypond_install_url(platform, version)
+        fn = Tempfile.new('lydown-lilypond-installer').path
 
-      Net::HTTP.start(url_base) do |http|
-        response = http.request_head(URI.escape(url_path))
-        pbar = ProgressBar.create(total: response['content-length'].to_i)
-        File.open(fn, 'w') {|f|
-          http.get(URI.escape(url_path)) do |str|
-            f.write str
-        @counter += str.length 
-        pbar.progress = @counter
+        download_lilypond(url, fn)
+        install_lilypond_files(fn, platform, version)
+      rescue => e
+        STDERR.puts "Failed to install lilypond #{version}"
+        exit(1)
+      end
+    
+      BASE_URL = "http://download.linuxaudio.org/lilypond/binaries"
+    
+      def lilypond_install_url(platform, version)
+        ext = platform =~ /darwin/ ? ".tar.bz2" : ".sh"
+        filename = "lilypond-#{version}-1.#{platform}"
+      
+        "#{BASE_URL}/#{platform}/#{filename}#{ext}"
+      end
+    
+      def detect_lilypond_platform
+        case RUBY_PLATFORM
+        when /x86_64-darwin/
+          "darwin-x86"
+        when /ppc-darwin/
+          "darwin-ppc"
+        when "i686-linux"
+          "linux-x86"
+        when "x86_64-linux"
+          "linux-64"
+        when "ppc-linux"
+          "linux-ppc"
+        end
+      end
+    
+      def download_lilypond(url, fn)
+        STDERR.puts "Downloading #{url}"
+      
+        url_base = url.split('/')[2]
+        url_path = '/'+url.split('/')[3..-1].join('/')
+        download_count = 0
+
+        Net::HTTP.start(url_base) do |http|
+          request_url = URI.escape(url_path)
+          response = http.request_head(request_url)
+          total_size = response['content-length'].to_i
+          pbar = ProgressBar.create(total: total_size)
+          File.open(fn, 'w') do |f|
+            http.get(request_url) do |data|
+              f << data
+              download_count += data.length 
+              pbar.progress = download_count if download_count <= total_size
+            end
           end
-         }
-        pbar.finish
+          pbar.finish
+        end
       end
-    end
     
-    def install_lilypond_files(fn, platform, version)
-      case platform
-      when /darwin/
-        install_lilypond_files_osx(fn, version)
+      def install_lilypond_files(fn, platform, version)
+        case platform
+        when /darwin/
+          install_lilypond_files_osx(fn, version)
+        when /linux/
+          install_lilypond_files_linux(fn, version)
+        end
       end
-    end
     
-    def install_lilypond_files_osx(fn, version)
-      target = "/tmp/lydown/installer/lilypond"
-      `mkdir -p #{target}`
+      def install_lilypond_files_osx(fn, version)
+        target = "/tmp/lydown/installer/lilypond"
+        FileUtils.mkdir_p(target)
       
-      STDERR.puts "Extracting..."
-      `tar -xjf #{fn} -C #{target}`
+        STDERR.puts "Extracting..."
+        exec "tar -xjf #{fn} -C #{target}"
       
-      copy_lilypond_files_osx(target, version)
-    end
+        copy_lilypond_files("#{target}/LilyPond.app/Contents/Resources", version)
+      end
+    
+      def install_lilypond_files_linux(fn, version)
+        target = "/tmp/lydown/installer/lilypond"
+        FileUtils.mkdir_p(target)
+      
+        # create temp directory in which to untar file
+        tmp_dir = "/tmp/lydown/#{Time.now.to_f}"
+        FileUtils.mkdir_p(tmp_dir)
+      
+        FileUtils.cd(tmp_dir) do
+          exec "sh #{fn} --tarball"
+        end
+      
+        STDERR.puts "Extracting..."
+        exec "tar -xjf #{tmp_dir}/#{fn} -C #{target}"
+      
+        copy_lilypond_files_linux("#{target}/usr", version)
+      end
 
-    def copy_lilypond_files_osx(archive, version)
-      base_path = "#{archive}/LilyPond.app/Contents/Resources"
+      def copy_lilypond_files(base_path, version)
+        target_dir = File.expand_path("~/.lydown/packages/lilypond/#{version}")
       
-      target_dir = File.expand_path("~/.lydown/packages/lilypond/#{version}")
+        # create directory for lilypond files
+        FileUtils.mkdir_p(target_dir)
       
-      # create directory for lilypond files
-      `mkdir -p #{target_dir}`
+        # copy files
+        %w{bin etc lib lib64 share var}.each do |entry|
+          dir = File.join(base_path, entry)
+          FileUtils.cp_r(dir, target_dir) if File.directory?(dir)
+        end
       
-      # copy files
-      %w{bin etc lib share}.each do |entry|
-        `cp -R #{File.join(base_path, entry)} #{target_dir}`
+        # create symlink
+        symlink_path = File.expand_path('~/bin/lilypond')
+        FileUtils.ln_sf("#{target_dir}/bin/lilypond", symlink_path)
+      
+        STDERR.puts `lilypond -v`
       end
       
-      # create symlink
-      symlink_path = File.expand_path('~/bin/lilypond')
-      `ln -Ffs #{target_dir}/bin/lilypond #{symlink_path}`
-      
-      STDERR.puts `lilypond -v`
+      def exec(cmd)
+        raise unless system(cmd)
+      end
     end
   end
 end
